c6d65b52175fe9bd2a47a0d83247fa00fb47810b
Ajima Fabian <ajimafabian18@gmail.com>
2025-10-02 16:42:17 -0700

    added some styling the login and signup form

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -8,7 +8,7 @@ const Signup = () => {
         <form className='flex flex-col space-y-4 mt-4'>
             <input type="text" placeholder='Enter email' className='outline-0 ring ring-blue-600 py-2 px-4 focus:border-0 focus:ring-2 focus:ring-blue-500 rounded-md'/>
             <input type="text" placeholder='Enter password' className='outline-0 ring ring-blue-600 py-2 px-4 focus:ring-2 focus:ring-blue-500 rounded-md'/>
-            <button className='text-lg p-2 border border-blue-600 rounded-md font-bold text-blue-600 hover:bg-blue-600 hover:text-white transition duration-300 tracking-wider'>Create</button>
+            <button className='text-lg p-2 bg-blue-600 rounded-md font-bold text-white hover:border-blue-600 hover:text-blue-600 hover:border hover:bg-white transition duration-300 tracking-wider'>Create</button>
         </form>
     </div>
   )
