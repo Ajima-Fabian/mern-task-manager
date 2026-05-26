208b03c9ed01a2603951bbabf3f544b528b5a7f5
Ajima-Fabian <ajimafabian18@gmail.com>
2026-05-26 17:58:26 +0100

    Use environment variables for admin credentials

--- backend/utils/createAdmin.js
+++ backend/utils/createAdmin.js
@@ -6,10 +6,11 @@ dotenv.config();
 
 const createAdmin = async () => {
     try {
-        // use production-safe env
         await mongoose.connect(process.env.MONGO_URI);
 
-        const existingAdmin = await User.findOne({ email: "admin@gmail.com" });
+        const existingAdmin = await User.findOne({
+            email: process.env.ADMIN_EMAIL
+        });
 
         if (existingAdmin) {
             console.log("Admin already exists");
@@ -18,8 +19,8 @@ const createAdmin = async () => {
 
         await User.create({
             name: "Admin",
-            email: "admin@gmail.com",
-            password: "admin1234",
+            email: process.env.ADMIN_EMAIL,
+            password: process.env.ADMIN_PASSWORD,
             role: "admin"
         });
 
