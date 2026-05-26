d0494b2f194dfbf029912a69d5f63416170f6d0c
Ajima-Fabian <ajimafabian18@gmail.com>
2026-05-26 18:03:55 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 import dotenv from "dotenv"
-dotenv.config()
 import app from "./app.js"
 import connectDB from "./config/db.js"
+import {createAdmin} from "./utils/createAdmin.js"
 
+dotenv.config()
 const PORT = process.env.PORT || 5000
 
 const startServer = async () => {
     try{
         await connectDB()
+        await createAdmin()
         app.listen(PORT,() => {
             console.log(`Server running on port: ${PORT}`)
         })
