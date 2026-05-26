67d89621a42c7eab8dc84f8eebb4c93fbf1fc838
Ajima-Fabian <ajimafabian18@gmail.com>
2026-05-26 18:07:30 +0100

    Update createAdmin.js

--- backend/utils/createAdmin.js
+++ backend/utils/createAdmin.js
@@ -1,17 +1,14 @@
-import mongoose from "mongoose";
 import User from "../models/UserModel.js";
 
 export const createAdmin = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-
         const existingAdmin = await User.findOne({
             email: process.env.ADMIN_EMAIL
         });
 
         if (existingAdmin) {
             console.log("Admin already exists");
-            process.exit(0);
+            return;
         }
 
         await User.create({
@@ -22,10 +19,8 @@ export const createAdmin = async () => {
         });
 
         console.log("Admin created successfully");
-        process.exit(0);
 
     } catch (error) {
         console.error("Error creating admin:", error.message);
-        process.exit(1);
     }
 };
